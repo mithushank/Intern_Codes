63e19f8d4b31ccbec19e45a21356e614a9cf9d6a
kovintharajan Mithushan <127074715+mithushank@users.noreply.github.com>
2024-02-22 11:28:04 +0800

    Update agglomative_algorithm.py

--- Buffer/buffer_update/agglomative_algorithm.py
+++ Buffer/buffer_update/agglomative_algorithm.py
@@ -18,32 +18,39 @@ class FeatureExtractor(nn.Module):
         return self.features(x)
 
 def agglomerative_cluster(B_t, M, k):
-    ''' 
+    '''
     Key arguments: New Batch(B_t), Buffer, budget (k)
     here B_t have n data points in the format (x, y)
-    
+
     '''
+    i=0
     for (x_i, y_i) in B_t:
+
         # Create an instance of the FeatureExtractor
         feature_extractor = FeatureExtractor(nn.Module)
         # Extract features
         a_i = feature_extractor(x_i)
         if len(M) < k:
-            M.append((x_i, y_i, a_i, 1))
-        
+            M.append([x_i, y_i, a_i, 1,i]) # here w=1
+
         else:
             j = min(range(len(M)), key=lambda j: np.linalg.norm(a_i - M[j][2]))
-
             a_prime = (M[j][3] * M[j][2] + a_i) / (1 + M[j][3])
-
             if np.linalg.norm(a_i - a_prime) <= np.linalg.norm(M[j][2] - a_prime):
-                M[j] = (x_i, y_i, a_prime, M[j][3])   
+                M[j] = [x_i, y_i, a_prime, M[j][3],i]
+            M[j][2]=a_prime     # feature update
+            M[j][2]+=1           # w update 
+
+        i+=1
     return M
 
 # Example usage:
 pretrained_model = models.resnet50(weights=True)
-B_t = [(torch.randn((3, 224, 224)), 0) for _ in range(10)]  # Replace this with your actual data
+B_t = [(torch.randn((3, 224, 224)), 0) for _ in range(100)]  # Replace this with your actual data
 M = []
-k = 10
+k = 50
 M = agglomerative_cluster(B_t, M, k)
-print(len(M))
+
+# To see which occurance of data points are in the buffer
+for i in M:
+  print(i[4])
